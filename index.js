63250a99288bfd82d070b2d6ce9a947d9abee29c
dperolio <dustin.perolio@outlook.com>
2021-02-27 14:14:29 -0500

    remove a no longer needed patch

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { findInReactTree, getReactInstance } from '@vizality/util/react';
+import { getReactInstance } from '@vizality/util/react';
 import { patch, unpatch } from '@vizality/patcher';
 import { CodeBlock } from '@vizality/components';
 import { getModule } from '@vizality/webpack';
@@ -23,29 +23,18 @@ export default class BetterCodeBlocks extends Plugin {
       this.injectCodeBlock(args, res);
       return res;
     });
-
-    patch('better-code-blocks-embed', parser, 'parseAllowLinks', (_, res) => {
-      for (const children of res) {
-        const codeblock = findInReactTree(children, n => n?.type?.name === '');
-        if (codeblock) {
-          this.injectCodeBlock(null, codeblock);
-        }
-      }
-      return res;
-    });
     this._forceUpdate();
   }
 
   injectCodeBlock (args, codeblock) {
-    const { render } = codeblock.props;
-
+    const { render } = codeblock?.props;
     codeblock.props.render = codeblock => {
       let lang, content, contentIsRaw, res;
       if (!args) {
         res = render(codeblock);
-        lang = res?.props?.children?.props?.className?.split(' ').find(className => !className.includes('-') && className !== 'hljs');
+        lang = res?.props?.children?.props?.className?.split(' ')?.find(className => !className.includes('-') && className !== 'hljs');
         if (res?.props?.children?.props?.children) {
-          content = res?.props?.children?.props?.children;
+          content = res.props.children.props.children;
         } else {
           content = res?.props?.children?.props?.dangerouslySetInnerHTML?.__html;
           contentIsRaw = true;
@@ -71,6 +60,8 @@ export default class BetterCodeBlocks extends Plugin {
      * @todo Make this better.
      * @note Some messages don't have onMouseMove, so check for that first.
      */
-    document.querySelectorAll(`[id^='chat-messages-']`).forEach(e => getReactInstance(e)?.memoizedProps?.onMouseMove && getReactInstance(e)?.memoizedProps?.onMouseMove());
+    document.querySelectorAll(`[id^='chat-messages-']`).forEach(e =>
+      Boolean(getReactInstance(e)?.memoizedProps?.onMouseMove) && getReactInstance(e).memoizedProps.onMouseMove()
+    );
   }
 }
